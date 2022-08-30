9347ecddd137b7400bb299e5dd077e83345e5e31
lawonyaungchyi1 <nyanlintun225nlt@gmail.com>
2022-08-30 21:25:45 +0630

    First Commit

---
+++ src/components/MyPage/MyPage.js
@@ -0,0 +1,38 @@
+import nyanlin from "./../nyanlin.png";
+import { Container, MessageBox } from "./MyPage-style";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import Header from "./../Header";
+import Form from "./../Form";
+
+const MyPage = () => {
+  const [click, setClick] = useState(false);
+
+  const showQuestions = () => {
+    setClick(!click);
+  };
+  return (
+    <>
+      {click ? (
+        <>
+          <Header />
+          <Form />
+        </>
+      ) : (
+        <div style={Container}>
+          <div>
+            <Button variant="contained" color="warning" onClick={showQuestions}>
+              မေးခွန်းကြည့်ကြရအောင်
+            </Button>
+          </div>
+          <MessageBox>
+            ""သားကြီး မင်းလည်းငါနဲ့အတူတူbotanyဌာနကိုသွားရမှာပါပဲ။""😂😂😂😂
+          </MessageBox>
+          <img src={nyanlin} alt="Nyan Lin Image" style={{ width: "240px" }} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MyPage;
